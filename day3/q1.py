68a97f9e6a9a6d20dae52912996f786eebabc1ee
Brent Jackson <jackson.w.brent@gmail.com>
2018-12-03 23:38:11 -0500

    Solved q2

--- day3/q1.py
+++ day3/q1.py
@@ -2,15 +2,16 @@ import re
 
 class Claim:
     def __init__(self,str):
-        regex = ".*@\s+(\d+),(\d+):\s+(\d+)x(\d+)"
+        regex = "#(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)"
         match = re.match(regex,str)
-        self.x = int(match.group(1))
-        self.y = int(match.group(2))
-        self.w = int(match.group(3))
-        self.h = int(match.group(4))
+        self.id = int(match.group(1))
+        self.x = int(match.group(2))
+        self.y = int(match.group(3))
+        self.w = int(match.group(4))
+        self.h = int(match.group(5))
 
     def __str__(self):
-        return "{},{} {}x{}".format(self.x, self.y, self.w, self.h)
+        return "#{} @ {},{} {}x{}".format(self.id, self.x, self.y, self.w, self.h)
 
 
 def show(grid):
@@ -19,7 +20,7 @@ def show(grid):
 
 
 
-def solve(lines):
+def solve_q1(lines):
 
     x_max = 0
     y_max = 0
@@ -48,7 +49,28 @@ def solve(lines):
     # grid[y][x] notation 
     # show(grid)
 
-    return total_overlaps
+    return total_overlaps, grid
+
+def solve_q2(grid, lines):
+    claims = [Claim(line) for line in lines]
+    for claim in claims:
+        print(claim)
+        unoverlapped = True
+        h = 0
+        while h < claim.h and unoverlapped:
+            w = 0
+            while w < claim.w and unoverlapped:
+                if grid[claim.y+h][claim.x+w] != 1:
+                    unoverlapped = False
+                    print("\toverlapped at {},{}".format(claim.x+w,claim.y+h))
+                w += 1
+            h += 1
+
+        if unoverlapped:
+            print("\tunoverlapped!")
+            return claim
+    return None
+
 
         
 def test():
@@ -56,10 +78,15 @@ def test():
         "#1 @ 0,0: 2x2",
         "#2 @ 1,1: 3x4",
         "#3 @ 2,2: 2x2",
-        "#4 @ 0,1: 3x6"        
+        "#4 @ 0,1: 3x6",
+        "#5 @ 5,1: 2x2"      
     ]
 
-    print(solve(claims))
+    q1,grid = solve_q1(claims)
+    show(grid)
+    print("overlapped_squares:",q1)
+
+    print("Unoverlapped_claim:",solve_q2(grid,claims))
 
 
 fname = "input.txt"
@@ -68,7 +95,12 @@ with open(fname) as f:
     lines = f.readlines()
 
 
-# test()
-print("Total overlapped squares:",solve(lines))
+test()
+
+q1, grid = solve_q1(lines)
+print("Total overlapped squares:",q1)
+
+q2 = solve_q2(grid, lines)
+print("unoverlapped claim:",q2)
 
 
